8835675122be5ffd930b73bfc3243529b461811d
matt <mdailis@ccs.neu.edu>
2018-04-19 13:44:24 +0200

    Started .h file

--- include/auxiliary.h
+++ include/auxiliary.h
@@ -5,3 +5,16 @@
  * @brief 	Headers for the auxiliary functions required by filesystem.c.
  * @date	01/03/2017
  */
+
+typedef struct SuperBlock {
+    int num_inodes;
+    int num_data_blocks;
+} SuperBlock;
+
+typedef struct INode {
+    int num_blocks;
+    void * blocks[];
+    int size;
+} INode;
+
+typedef char[256] AllocBlock;
